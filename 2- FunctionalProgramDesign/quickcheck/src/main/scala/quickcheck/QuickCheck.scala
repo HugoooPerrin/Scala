3f8abb4f48ffe119b1b8b771a18603e02d88dc01
HugoooPerrin <b00605838@essec.edu>
2018-04-10 23:44:16 +0200

    Quickcheck assignment

--- 2- FunctionalProgramDesign/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ 2- FunctionalProgramDesign/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,12 +9,22 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    priority <- arbitrary[A]
+    heap <- frequency((1, Gen.const(empty)), (9, genHeap))
+  } yield insert(priority, heap)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  property("gen1") = forAll { (h: H) =>
-    val m = if (isEmpty(h)) 0 else findMin(h)
-    findMin(insert(m, h)) == m
+  property("gen6") = forAll { (heap1: H, heap2: H) =>
+    def heapEqual(heap1: H, heap2: H): Boolean =
+      if (isEmpty(heap1) && isEmpty(heap2)) true
+      else {
+        val m1 = findMin(heap1)
+        val m2 = findMin(heap2)
+        m1 == m2 && heapEqual(deleteMin(heap1), deleteMin(heap2))
+      }
+    heapEqual(meld(heap1, heap2),
+      meld(deleteMin(heap1), insert(findMin(heap1), heap2)))
   }
-
 }
