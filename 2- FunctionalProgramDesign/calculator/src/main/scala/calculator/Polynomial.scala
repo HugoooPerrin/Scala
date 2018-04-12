5e65f9331de0451186ce6f6cf576ef4c05c9a6b9
HugoooPerrin <b00605838@essec.edu>
2018-04-12 18:57:51 +0200

    2/3 of calculator assignment

--- 2- FunctionalProgramDesign/calculator/src/main/scala/calculator/Polynomial.scala
+++ 2- FunctionalProgramDesign/calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,18 @@
 package calculator
 
+import scala.math.sqrt
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal(b()*b() - 4*a()*c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal(computeDelta(a, b, c)() match {
+      case n if n < 0 => Set()
+      case n if n >= 0 => Set((-b() + sqrt(n)) / (2 * a()), (-b() - sqrt(n)) / (2 * a()))
+    })
   }
 }
